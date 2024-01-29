b3f8ac64c99e6a62d80eb6f8dae371dbad5394f0
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2024-01-29 16:41:00 +0200

    c

--- KKCK_tasks/header.php
+++ KKCK_tasks/header.php
@@ -3,31 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 2: Create universal header, footer, or sidebar as per your layout done in the previous course.</title>
+    <title><?php echo $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     
-    <div class="container">
+    <div class="Container">
+        <header>
+        
+            <div class="row">
+                <div class="col-md-4">
+                     <h2>Chalani Kalpana</h2>
+                </div>   
 
-      <div class="row">
-        <div class="col-md-4"><h1>Chalani Kalpana</h1></div>
+    <div class="col-md-8">         
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+             
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="ex1.php">Exercise 01</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="ex2.php">Exercise 02</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li>
+                  
+                 
+                </ul>
+                
+              </div>
+            </div>
+          </nav>
 
-      <div class="col-md-8">
+    </div>
+            </div>
 
-        <ul class="nav nav-pills nav-fill">
-          <li class="nav-item">
-            <a class="nav-link " aria-current="page" href="#">CV</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Cover Letter</a>
-          </li>
-          <li class="nav-item">
-           <a class="nav-link" href="#">Social media Link</a>
-          </li>
-       
-        </ul>
-     </div>
-     
-     <header>
+
+        </header>
